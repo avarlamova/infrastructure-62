8ecec9173c84d5e4387f386b11a48850daa1227e
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-08-19 22:15:13 +0300

    test

---
+++ src/updateTicket.js
@@ -0,0 +1,33 @@
+const fetch = require("node-fetch")
+const url = "https://api.tracker.yandex.net/v2/issues/INFRA-62"
+const {OAUTH_TOKEN,AUTHOR} = process.env
+// let OAUTH_TOKEN = 'y0_AgAAAABG_epAAAhWcgAAAADMpApoGD8sXOqJQKS1ZCEdxrI1mAVPDW0';
+let headersList = {
+    "X-Org-ID": "7261414",
+    "Authorization": `OAuth ${OAUTH_TOKEN}`,
+    "Content-Type": "application/json"
+}
+//
+// async function fetchToken() {
+//     const response = await fetch(url,{
+//         method: "GET",
+//         headers: headersList
+//     })
+//     const ticket = await response.json()
+//     console.log(ticket)
+// }
+
+updateTicket();
+
+async function updateTicket() {
+    const updateResponse = await fetch(url,{
+        method: "PATCH",
+        headers: headersList,
+        body: {
+            "description": `Ответственный за релиз ${AUTHOR}`
+        }
+    })
+
+}
+
+// console.log(fetchToken())
