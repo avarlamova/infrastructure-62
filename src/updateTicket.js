1587b69001664e113b7ea7927bd1fa7cd0164b0b
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-08-20 14:25:37 +0300

    change config

--- src/updateTicket.js
+++ src/updateTicket.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch")
 const url = "https://api.tracker.yandex.net/v2/issues/INFRA-62"
-const {OAUTH_TOKEN, TAG_NAME, AUTHOR, COMMITS} = process.env
+const {OAUTH_TOKEN, TAG_NAME, AUTHOR, COMMITS, BEFORE, AFTER} = process.env
 let headersList = {
     "X-Org-ID": "7261414",
     "Authorization": `OAuth ${OAUTH_TOKEN}`,
@@ -22,7 +22,7 @@ async function updateTicket() {
     const formatter = new Intl.DateTimeFormat("ru");
     const currentDate = formatter.format(new Date())//.toLocaleDateString()
 
-    console.log(AUTHOR, TAG_NAME, COMMITS)
+    console.log(AUTHOR, TAG_NAME, COMMITS, BEFORE, AFTER)
     await fetch(url,{
         method: "PATCH",
         headers: headersList,
