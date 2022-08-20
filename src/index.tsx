49508532260d4383548bdc929f1112b3c051a230
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-08-20 14:18:25 +0300

    first

--- src/index.tsx
+++ src/index.tsx
@@ -19,4 +19,5 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+
 //  run: ./docker-build.sh ${{ github.head_ref }}.${{ github.sha }}
