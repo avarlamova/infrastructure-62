5b745e0155f6b3581cdc8a0e25e0b0f6cac47dca
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-08-20 21:53:18 +0300

    e

--- src/test.sh
+++ src/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LOG="git log rc.0.0.5"
 export COMMITS=$(eval $LOG)
-echo "commits<<EOF" >> $GITHUB_ENV
+echo "COMMITS<<EOF" >> $GITHUB_ENV
 echo "$ (eval $LOG)" >> $GITHUB_ENV
 echo "EOF" >> $GITHUB_ENV
 #echo "COMMITS=$(eval $LOG)" >> $GITHUB_ENV
