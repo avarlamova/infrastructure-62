c6ea16b1b59602c7a0e4d1c29719a4e8591acaea
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-08-21 19:15:53 +0300

    config

--- src/test.sh
+++ src/test.sh
@@ -2,16 +2,17 @@
 TAGS_COUNT=$( git tag | wc -l)
 LOG="git log --pretty=format:'%H %an %s'"
 
-#if [ $TAGS_COUNT -gt 1 ]
-#then
-LAST_TAG=$(git describe --abbrev=0 --match 'rc-0.*' --exclude $TAG_NAME)
-#  if [ $LAST_TAG != '' ]
-#   then
-    LOG="$LOG $LAST_TAG..HEAD"
-#    fi
-#else
-#    LOG="$LOG $TAG_NAME"
-#fi
+if [ $TAGS_COUNT -gt 1 ]
+then
+#LAST_TAG=$(git describe --abbrev=0 --match 'rc-0.*' --exclude $TAG_NAME)
+LAST_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1  --max-count=1`)
+  if [ $LAST_TAG != '' ]
+   then
+    LOG="$LOG $LAST_TAG...$TAG_NAME"
+    fi
+else
+    LOG="$LOG $TAG_NAME"
+fi
 
 export COMMITS=$(eval $LOG)
 echo "COMMITS<<EOF" >> $GITHUB_ENV
