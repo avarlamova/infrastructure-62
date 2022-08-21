aa74f5c7104dbbc0de491af427404a7b602fbe5a
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-08-21 10:31:47 +0300

    config

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-LOG="git log $TAG_NAME --pretty"
+LOG="git log --pretty=format:'%H %an %s'\n"
 LAST_TAG=$(git describe --abbrev=0 --match 'rc-0.*' --exclude $TAG_NAME)
+echo $LAST_TAG
 if [ $LAST_TAG != '' ]
  then
   LOG="$LOG $LAST_TAG..HEAD"
   fi
+echo $LOG
 export COMMITS=$(eval $LOG)
 echo "COMMITS<<EOF" >> $GITHUB_ENV
 echo "$COMMITS" >> $GITHUB_ENV
 echo "EOF" >> $GITHUB_ENV
 #echo "COMMITS=$(eval $LOG)" >> $GITHUB_ENV
 #  echo $COMMITS
-#--pretty=format:'%H %an %s'
-
 
